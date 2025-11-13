e13ac56245f61b7729fd9a2e0a0230f770cf9186
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-11-12 23:21:34 -0300

    feat: componente title

---
+++ src/components/title/index.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+import { StyledTitle } from "./styles"
+
+interface TitleProps {
+  children?: ReactNode;
+}
+
+export const Title = ({ children } : TitleProps) => {
+    return (
+        <StyledTitle>{children}</StyledTitle>
+    );
+}
---
+++ src/components/title/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledTitle = styled.h1`
+    font-family: "Poppins", sans-serif;
+    font-size: 3.5rem;
+    color: #333;
+    font-weight: 600;
+`;

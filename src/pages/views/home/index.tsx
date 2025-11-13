c290a73385e9254a6243d769472dfc4e3b54b861
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-11-12 23:28:52 -0300

    feat: ajuste no container home

--- src/pages/views/home/index.tsx
+++ src/pages/views/home/index.tsx
@@ -5,10 +5,10 @@ import { StyledImage } from "./styles";
 export const HomePage = () => {
     return (        
         <Container direction="row">
-            <Container width="49%">
+            <Container width="49%" alignItems="center" padding="10px">
                 <Title>Gabriel <br /> Batemarque</Title>
             </Container>
-            <Container width="49%" justifyContent="center" direction="row" padding="10px">
+            <Container width="49%" alignItems="center" padding="10px">
                 <StyledImage src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="dale" />
             </Container>
         </Container>

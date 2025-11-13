fc5c7a139514b8a4b11ab0fbac8783bdf4bae843
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-11-12 23:22:39 -0300

    feat: criando home

--- src/pages/views/home/index.tsx
+++ src/pages/views/home/index.tsx
@@ -1,9 +1,16 @@
 import { Container } from "../../../components/container";
+import { Title } from "../../../components/title";
+import { StyledImage } from "./styles";
 
 export const HomePage = () => {
     return (        
-        <Container>
-            <p>opaopaopa</p>
+        <Container direction="row">
+            <Container width="49%">
+                <Title>Gabriel <br /> Batemarque</Title>
+            </Container>
+            <Container width="49%" justifyContent="center" direction="row" padding="10px">
+                <StyledImage src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="dale" />
+            </Container>
         </Container>
     );
 }

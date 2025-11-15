86945bcbbe3abfe2361bf4cce73839d7236543db
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-11-14 21:01:48 -0300

    feat: redes sociais

--- src/pages/views/home/index.tsx
+++ src/pages/views/home/index.tsx
@@ -2,9 +2,38 @@ import { ActionButton } from "../../../components/actionButton";
 import { Container } from "../../../components/container";
 import { Text } from "../../../components/text";
 import { Title } from "../../../components/title";
-import { StyledImage } from "./styles";
+import { SocialMediaContainer, StyledImage } from "./styles";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 
 export const HomePage = () => {
+
+    const socialMedias = [
+        {
+            id: 0,
+            name: 'Instagram',
+            icon: <FaInstagramSquare />
+        },
+        {
+            id: 0,
+            name: 'Facebook',
+            icon: <FaFacebook />
+        },
+                {
+            id: 0,
+            name: 'Linkedin',
+            icon: <FaLinkedin />
+        },
+                {
+            id: 0,
+            name: 'GitHub',
+            icon: <FaGithub />
+        },
+    ]
+
     return (        
         <Container direction="row">
             <Container width="49%" alignItems="center" padding="10px">
@@ -15,8 +44,11 @@ export const HomePage = () => {
                         <ActionButton>bora fio bill</ActionButton>
                 </Container>                
             </Container>
-            <Container width="49%" alignItems="center" padding="10px">
+            <Container width="49%" alignItems="center" padding="10px" gap="8rem">
                 <StyledImage src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="dale" />
+                <SocialMediaContainer>
+                    {socialMedias?.map(socialMedia => (socialMedia?.icon))}
+                </SocialMediaContainer>
             </Container>
         </Container>
     );
--- src/pages/views/home/styles.ts
+++ src/pages/views/home/styles.ts
@@ -4,3 +4,8 @@ export const StyledImage = styled.img`
     border-radius: 50%;
     width: 400px;
 `;
+
+export const SocialMediaContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`;
